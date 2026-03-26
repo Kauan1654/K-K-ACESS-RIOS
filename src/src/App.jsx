5b2f1ff358009378bd6a60d0ca515d9b8f1477c2
Kauan1654 <marciokauanferre081@gmail.com>
2026-03-26 14:51:20 -0300

    Delete src/src/App.jsx

--- src/src/App.jsx
+++
@@ -1,8 +0,0 @@
-export default function App() {
-  return (
-    <div style={{ fontFamily: 'Arial', padding: 40 }}>
-      <h1>K & K ACESSÓRIOS</h1>
-      <p>Site pronto. Agora é só substituir pelos teus produtos.</p>
-    </div>
-  )
-}
